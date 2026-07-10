49820f7618d25563187f6bd1f344dfbf04e6cae8
Thomas Palma <thomaspalma777@gmail.com>
2026-07-10 18:25:31 -0300

    [refactor] update fastapi application metadata
    - update FastAPI app metadata in main;
    - remove default read_root endpoint from main.

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="Observability chaos lab with FastAPI",
+    version="0.1.0",
+    description="The application simulates a chaos orchestrator by generating different scenarios and events that produce structured logs, which are then collected and analyzed through the ELK Stack (Elasticsearch, Logstash, and Kibana). Its purpose is to provide a controlled environment for exploring log aggregation, search, visualization, and operational monitoring.",
+)
+
